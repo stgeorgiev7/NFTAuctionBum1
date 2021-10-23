9e0595f04f10b2388e14f0be740170034f9b3b32
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-10-23 20:08:49 +0300

    added Auction component

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -5,6 +5,7 @@ import styles from "./Example.module.scss";
 import classNames from "classnames";
 import Head from "../header/Header";
 import Trending from "../trending/Trending";
+import Auctions from "../auctions/Auctions";
 import userImage from '../avatar/image/avatar.png'
 import mediaImage from "../card/image/nft.jpg";
 
@@ -45,8 +46,43 @@ export default function Copyright() {
           }
         ]
       } />
-      {/* <Trending /> */}
-
+     
+     <Auctions cards={
+        [
+          {
+            name: "Clock",
+            user: { avatarUrl: userImage.src, verified: true },
+            mediaUrl: mediaImage.src,
+            price: 1,
+            currency: "ETH",
+            timeLeft: 12000,
+          },
+          {
+            name: "DOGE",
+            user: { avatarUrl: userImage.src, verified: true },
+            mediaUrl: mediaImage.src,
+            price: 2.3,
+            currency: "ETH",
+            timeLeft: 6000,
+          },
+          {
+            name: "BTC",
+            user: { avatarUrl: userImage.src, verified: true },
+            mediaUrl: mediaImage.src,
+            price: 5,
+            currency: "ETH",
+            timeLeft: 12000,
+          },
+          {
+            name: "Litecoin",
+            user: { avatarUrl: userImage.src, verified: true },
+            mediaUrl: mediaImage.src,
+            price: 10,
+            currency: "ETH",
+            timeLeft: 24000,
+          }
+        ]
+      } />
       <Container className={classNames(styles.container)} maxWidth="xl">
 
 
